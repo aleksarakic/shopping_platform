3ccb8aa1c8b0e6c2f72c67928466c6d01c7235f0
Aleksa <rakicaleksa@ymail.com>
2020-03-07 01:29:52 +0100

    Added first product spec

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to have_db_column(:name).of_type(:string) }
+  it { is_expected.to have_db_column(:description).of_type(:string) }
+  it { is_expected.to have_db_column(:price).of_type(:string) }
+  it { is_expected.to have_db_column(:quantity).of_type(:string) }
+  it { is_expected.to have_db_column(:category).of_type(:string) }
+
+  it { is_expected.to belong_to(:supplier) }
 end
